77ee626d78e38b497125e212ba1a7fd9da8826db
Ibadet02 <ibadetismayilov20@gmail.com>
2025-12-23 22:08:44 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+import AppStyles from "./styles/App.module.css";
+
+export default function App() {
+  return (
+    <div className={AppStyles.myBtn}>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
